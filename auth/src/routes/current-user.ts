16f669f66dd2568afad1da328bc82eb2db4a56fd
Carmencc <carmencai5114@gmail.com>
2025-02-28 10:49:04 -0500

    Auth: get-user & authorization middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { getCurrentUser } from "../middlewares/get-current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  // 1. Check if there is a jwt in cookie
-  if (!req.session?.jwt) {
-    res.send({ currentUser: null });
-    return;
-  }
-
-  // 2. Verify if jwt is valid
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    // if the jwt has been tempered, verify() will throw an error
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", getCurrentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
