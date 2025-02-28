a0d356b42afff7e7a534f7afc2ce03bda96d6f5a
Carmencc <carmencai5114@gmail.com>
2025-02-27 23:51:32 -0500

    Auth: get current user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  // 1. Check if there is a jwt in cookie
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null });
+    return;
+  }
+
+  // 2. Verify if jwt is valid
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    // if the jwt has been tempered, verify() will throw an error
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
