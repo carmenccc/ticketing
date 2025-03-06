5407ab1b0e0d402f1b51313b7ed7524b2a590eb3
Carmencc <carmencai5114@gmail.com>
2025-03-05 22:52:56 -0500

    Next: Header component

---
+++ client/components/header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return <li key={href}>{label}</li>;
+    });
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div>
+        <Link href="/" className="navbar-brand">
+          {/* <a className="navbar-brand">GitTix</a> */}
+          GitTix
+        </Link>
+      </div>
+
+      <div classNmae="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
